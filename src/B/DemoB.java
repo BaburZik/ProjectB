2b36d5f8d0c8394f4d70b24dd55ed06045b70f27
babur <babur@BABURlaptop.fios-router.home>
2024-01-03 20:50:21 -0500

    Initial Commit

---
+++ src/B/DemoB.java
@@ -0,0 +1,11 @@
+package B;
+
+public class DemoB {
+	public static void main(String[] args) {
+		System.out.println("tuesday");
+		
+		System.out.println("Day1");
+		System.out.println("Day2");
+	}
+
+}
